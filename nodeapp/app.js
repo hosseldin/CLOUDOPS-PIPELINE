4db9ca20837f4087413510926d85ee4b28a3a086
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2025-04-30 04:30:26 +0300

    trigger test x8

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -43,7 +43,7 @@ app.get('/redis', (req, res) => {
         }
         if (rep) {                          //JSON objects need to be parsed after reading from redis, since it is stringified before being stored into cache                      
             console.log(rep);
-            res.send("Redis is successfully connected HOSdasAA and mourad");
+            res.send("Redis is successfully connected HOSdasdsAA and mourad");
         }
     })
 })
