e81200f9bf25aa141535be368d814ee5754b1d40
Mourad <mohamed0mourad@gmail.com>
2025-05-01 19:04:15 +0300

    Add environment.groovy

---
+++ Jenkins-kaniko/environment.groovy
@@ -0,0 +1,7 @@
+// environment.groovy
+return [
+  AWS_REGION: 'us-east-1',
+  ECR_REGISTRY: '773893527461.dkr.ecr.us-east-1.amazonaws.com',
+  ECR_REPOSITORY: 'node-app-jenkins',
+  TARGET_FOLDER: 'nodeapp'
+]
